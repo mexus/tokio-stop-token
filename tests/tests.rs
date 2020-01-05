69dffd7f34f5ad4cf6370ae87effd4047decbbec
mexus <gilaldpellaeon@gmail.com>
2020-01-06 00:17:45 +0300

    Cargo.toml: fix authors, description and repo link

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,10 +2,7 @@ use std::time::Duration;
 
 use async_stream::stream;
 use futures_intrusive::channel::shared::{channel, Receiver};
-use tokio::{
-    stream::{Stream, StreamExt},
-    time::delay_for,
-};
+use tokio::stream::{Stream, StreamExt};
 
 use stop_token::StopSource;
 
@@ -17,35 +14,54 @@ fn into_stream(receiver: Receiver<i32>) -> impl Stream<Item = i32> {
     })
 }
 
+mod task {
+    use std::{future::Future, time::Duration};
+
+    pub fn block_on<F: Future>(f: F) -> F::Output {
+        tokio::runtime::Runtime::new().unwrap().block_on(f)
+    }
+
+    pub async fn spawn<F>(f: F) -> F::Output
+    where
+        F: Future + Send + 'static,
+        F::Output: Send,
+    {
+        tokio::spawn(f).await.unwrap()
+    }
+
+    pub async fn sleep(duration: Duration) {
+        tokio::time::delay_for(duration).await
+    }
+}
+
 #[test]
 fn smoke() {
-    tokio::runtime::Runtime::new().unwrap().block_on(async {
+    task::block_on(async {
         let (sender, receiver) = channel::<i32>(10);
         let stop_source = StopSource::new();
-        let task = tokio::spawn({
+        let task = task::spawn({
             let stop_token = stop_source.stop_token();
             let receiver = receiver.clone();
             let receiver = into_stream(receiver);
             async move {
-                let mut xs = Vec::new();
-                let mut stream = stop_token.stop_stream(receiver);
-                while let Some(x) = stream.next().await {
-                    xs.push(x)
-                }
-                xs
+            let mut xs = Vec::new();
+            let mut stream = stop_token.stop_stream(receiver);
+            while let Some(x) = stream.next().await {
+                xs.push(x)
             }
-        });
+            xs
+        }});
         sender.send(1).await.unwrap();
         sender.send(2).await.unwrap();
         sender.send(3).await.unwrap();
 
-        delay_for(Duration::from_millis(250)).await;
+        task::sleep(Duration::from_millis(250)).await;
         drop(stop_source);
-        delay_for(Duration::from_millis(250)).await;
+        task::sleep(Duration::from_millis(250)).await;
 
         sender.send(4).await.unwrap();
         sender.send(5).await.unwrap();
         sender.send(6).await.unwrap();
-        assert_eq!(task.await.unwrap(), vec![1, 2, 3]);
+        assert_eq!(task.await, vec![1, 2, 3]);
     })
 }
